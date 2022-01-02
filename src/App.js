2c7278315b8e90e69ba839b2938a62b772f82159
Pushpendra Sharma <79936276+Pushpendra-Sharma@users.noreply.github.com>
2022-01-02 18:53:00 +0530

    feat: added useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-var headingText = "welcome";
-var headingStyle = { color: "red" };
-
-var userName = "Pushpendra";
-
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function clickHandler() {
+    var newValue = counter + 1;
+    setCounter(newValue);
+  }
+
+  console.log("clicked", counter);
+
   return (
     <div>
-      <h1>
-        {headingText} <span style={headingStyle}>{userName}</span>
-      </h1>
+      <h1>Emoji-interpreter</h1>
+      <button onClick={clickHandler}>Click here</button> {counter}
     </div>
   );
 }
